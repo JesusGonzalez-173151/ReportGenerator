554a928eee908171f42b730a65e6a8eebe9a2bfa
JesusGonzalez-173151 <jesusi.gonzalez@potros.itson.edu.mx>
2021-07-25 17:06:35 -0700

    cambios

--- src/main/java/com/microservice/reportGenerator/ReportGeneratorApplication.java
+++ src/main/java/com/microservice/reportGenerator/ReportGeneratorApplication.java
@@ -19,22 +19,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReportGeneratorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReportGeneratorApplication.class, args);
-//        try {
-//            givenInvalidInput_whenValidating_thenInvalid();
-//            System.out.println("pasó");
-//        } catch (Exception e) {
-//            System.out.println("no pasó");
-//        }
-//        
+        //SpringApplication.run(ReportGeneratorApplication.class, args);
+        try {
+            givenInvalidInput_whenValidating_thenInvalid();
+            System.out.println("pasó");
+        } catch (Exception e) {
+            System.out.println("no pasó");
+        }
+        
     }
 //    private static InputStream inputStreamFromClasspath(String path) {
 //        return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
 //    }
     @Test
     public static void givenInvalidInput_whenValidating_thenInvalid() throws ValidationException, FileNotFoundException {
-        InputStream inputstream = new FileInputStream("C:\\Users\\USER\\Documents\\NetBeansProjects\\ReportGenerator\\src\\main\\resources\\static\\jsonSchema.json");
-        InputStream inputstream2 = new FileInputStream("C:\\Users\\USER\\Documents\\NetBeansProjects\\ReportGenerator\\src\\main\\resources\\static\\jsonDatas.json");
+        InputStream inputstream = new FileInputStream("C:\\Users\\Administrator\\Desktop\\ReportGenerator\\src\\main\\resources\\static\\jsonSchema.json");
+        InputStream inputstream2 = new FileInputStream("C:\\Users\\Administrator\\Desktop\\ReportGenerator\\src\\main\\resources\\static\\jsonDatas.json");
 
         JSONObject jsonSchema = new JSONObject(
                 new JSONTokener(inputstream));
